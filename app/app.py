c8d5b0e1b65b860e77b691d8f92374e6ddd6f4ba
Usama Subhani <reachusama@protonmail.com>
2020-05-18 16:11:48 +0500

    Remove unused imports

--- app/app.py
+++ app/app.py
@@ -2,17 +2,14 @@
 from flask import Flask, request, render_template
 import numpy as np
 from tensorflow.keras.models import load_model, model_from_json
-import matplotlib.image
 from skimage.transform import resize
-# from scipy.misc import imread
-
 import base64
 import json
 from base64 import b64decode
-import re
 import cv2
 
 app = Flask(__name__)
+# Load Model
 json_file = open('../model_v0.2.json', 'r')
 model_json = json_file.read()
 json_file.close()
@@ -31,14 +28,13 @@ def Predict():
     img_data = request.get_data().decode('utf-8')
     # Separate the metadata from the image data
     head, data = img_data.split(',', 1)
-    # Write the image to a file
+    # Save Image
     with open('image.png', 'wb') as f:
         f.write(base64.b64decode(data))
-
+    # Read saved image
     img = cv2.imread('image.png')[:,:,0]
     img = cv2.resize(img, (28,28), interpolation = cv2.INTER_AREA)
     img = np.invert(np.array([img]))
-    print(img.shape)
     prediction = model.predict_classes(img)
     return np.array2string(prediction[0])
     
